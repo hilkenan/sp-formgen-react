134822e46c8f25ee18a58d51da33ed65615fd311
Andreas Hilken <andreas.hilken@did-c.com>
2018-05-17 08:33:20 +0200

    Proxy

--- src/proxy.js
+++ src/proxy.js
@@ -3,10 +3,6 @@ const RestProxy = require('sp-rest-proxy');
 const settings = {  
   port: 4323,
   protocol: 'http',
-  // ssl: {
-  //   cert: CertificateStore.default.instance.certificateData,
-  //   key: CertificateStore.default.instance.keyData
-  // }};
 }
 const restProxy = new RestProxy(settings);  
 restProxy.serve();  
